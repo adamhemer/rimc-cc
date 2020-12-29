74e3cb82d5fccbfa535fc126173c3705b9b931f5
Adam Hemer <adamhemer32@gmail.com>
2020-12-30 00:31:48 +1030

    client

--- client.lua
+++ client.lua
@@ -12,3 +12,18 @@ for i = 0, w do
     end
 end
 
+_corner = "+"
+_sides = "|"
+
+function drawBox(x, y, w, h)
+    for j = y, h do
+        for i = x, w do
+            term.setCursorPos(i, j)
+            if i == x or i == w then
+                term.write(_corner)
+            else
+                term.write(_sides)
+            end
+        end
+    end
+end
